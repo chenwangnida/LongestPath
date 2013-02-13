c6dcbf15a8dd88edfbc5e0a17fb59de937abe85a
Clark Gredona <cgredona@gmail.com>
2013-02-13 01:20:00 -0500

    Added an option of using a different file as input

--- src/yodletriangle/YodleTriangle.java
+++ src/yodletriangle/YodleTriangle.java
@@ -9,21 +9,31 @@ import java.io.IOException;
 import org.jgrapht.graph.*;
 
 public final class YodleTriangle {
-    
+
     /**
      * The starting point for the demo.
      *
      * @param args ignored.
      */
     public static void main(String[] args) throws FileNotFoundException, IOException {
-        File f = new File("triangle.txt");
+        File f = null;
+        if (args.length == 0) {
+            f = new File("triangle.txt");
+        } else if (args.length == 1) {
+            f = new File(args[0]);
+        } else if (args.length > 1) {
+            System.err.println("Input syntax: java -jar YodleTriangle.jar");
+            System.err.println("OR");
+            System.err.println("java -jar YodleTriangle.jar <fileName>");
+            System.exit(0);
+        }
 
         TriangleGraph<GraphInteger, DefaultWeightedEdge> testGraph = TriangleGraph.createTriangleGraph(f);
-        
+
 
         System.out.println("This is a representation of the graph:");
         System.out.println(testGraph);
-        
+
         System.out.println();
         System.out.print("The length of the longest path from top to bottom is ");
         System.out.println(TriangleGraphAlg.getLongestPath(testGraph));;
